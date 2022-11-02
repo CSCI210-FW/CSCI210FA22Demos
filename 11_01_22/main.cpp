5e48bd703fa87bcff301a7d7cd621930b5a5128d
Angela Venable <avenable11@ivytech.edu>
2022-11-01 20:39:02 -0400

    callback functions

--- 11_01_22/main.cpp
+++ 11_01_22/main.cpp
@@ -4,13 +4,14 @@
 
 using namespace std;
 
+int callback(void*, int, char**, char**);
 
 int main()
 {
     sqlite3 * db;
     int rc;
 
-    rc  = sqlite3_open_v2("ConstructCo1.db", &db, SQLITE_OPEN_READWRITE, NULL);
+    rc  = sqlite3_open_v2("ConstructCo.db", &db, SQLITE_OPEN_READWRITE, NULL);
     if(rc != SQLITE_OK)
     {
         cout << "Error opening database: " << sqlite3_errmsg(db) << endl;
@@ -22,7 +23,30 @@ int main()
         cout << "Database opened successfully." << endl;
     }
 
+    string query = "select emp_num as 'Employee Number', emp_fname || ' ' || emp_lname as 'Employee Name' from employee";
+    char * err;
+    rc = sqlite3_exec(db, query.c_str(), callback, NULL, &err);
+    if(rc != SQLITE_OK)
+    {
+        cout << "There was an error - select callback: " << err << endl;
+        sqlite3_free(err);
+    }
 
     sqlite3_close(db);
     return 0;
+}
+
+
+int callback(void* data, int numCols, char** values, char** columnNames)
+{
+    for(int i = 0; i < numCols; i++)
+    {
+        cout << columnNames[i] << ": ";
+        if(values[i] != NULL)
+            cout << values[i];
+
+        cout << endl;
+    }
+    cout << endl;
+    return SQLITE_OK;
 }
---
+++ 11_01_22/main2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include "sqlite3.h"
+
+using namespace std;
+
+int movieCallback(void*, int, char**, char**);
+int actorCallback(void*, int, char**, char**);
+
+int main()
+{
+    sqlite3 * db;
+    int rc;
+
+    rc  = sqlite3_open_v2("IMDB.db", &db, SQLITE_OPEN_READWRITE, NULL);
+    if(rc != SQLITE_OK)
+    {
+        cout << "Error opening database: " << sqlite3_errmsg(db) << endl;
+        sqlite3_close(db);
+        return 0;
+    }
+    else
+    {
+        cout << "Database opened successfully." << endl;
+    }
+
+    string query = "select distinct movie.name as 'Movie', movie.year as 'Year', movie.id ";
+    query += "from movie ";
+    query += "where movie.name like \"Shrek%\"";
+    cout << query << endl;
+    char * err;
+    rc = sqlite3_exec(db, query.c_str(), movieCallback, db, &err);
+    if(rc != SQLITE_OK)
+    {
+        cout << "There was an error - select callback: " << err << endl;
+        sqlite3_free(err);
+    }
+
+    sqlite3_close(db);
+    return 0;
+}
+
+
+int movieCallback(void* data, int numCols, char** values, char** columnNames)
+{
+    for(int i = 0; i < numCols - 1; i++)
+    {
+        cout << columnNames[i] << ": ";
+        if(values[i] != NULL)
+            cout << values[i];
+
+        cout << endl;
+    }
+    cout << "Cast:" << endl;
+    sqlite3 * db = (sqlite3 *) data;
+    string query = "select actor.first_name || ' ' || actor.last_name || ' - ' || c.role as actorinfo ";
+    query += "from actor join cast as c on actor.id = c.actor_id ";
+    query += "where c.movie_id = ";
+    query += values[numCols -1];
+    char * err;
+    int rc = sqlite3_exec(db, query.c_str(), actorCallback, NULL, &err);
+    if(rc != SQLITE_OK)
+    {
+        cout << "There was an error - actor callback: " << err << endl;
+        cout << query << endl;
+        sqlite3_free(err);
+        return SQLITE_ERROR;
+    }
+    cout << endl;
+    return SQLITE_OK;
+}
+
+int actorCallback(void* data, int numCols, char** values, char** columnNames)
+{
+    if(values[0] != NULL)
+        cout << values[0];
+    cout << endl;
+    return SQLITE_OK;
+}
